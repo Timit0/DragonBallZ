673ae6a7b2e1723367a80dcba59f73ec8ba402ff
TimHaCeff <tim.ha@ceff.ch>
2024-08-22 08:55:04 +0200

    Input map can be changed

--- Scenes/Menus/OptionsMenu/Tables/Controls/Button/States/RemapButtonNormalState.cs
+++ Scenes/Menus/OptionsMenu/Tables/Controls/Button/States/RemapButtonNormalState.cs
@@ -5,6 +5,11 @@ public partial class RemapButtonNormalState : BasicRemapStateBase
 {
     public override void Enter()
     {
+        this.buttonToggleAction.Pressed += on_buttonToggleAction_Pressed;
+        // this.buttonToggleAction.Text = InputMap.Singleton.ActionGetEvents(this.buttonToggleAction.ActionName)[0].ToString();
+        string inputName = InputMap.Singleton.ActionGetEvents(this.buttonToggleAction.ActionName)[0].AsText();
+        this.buttonToggleAction.Text = inputName;
+
         base.Enter();
     }
 
@@ -15,6 +20,12 @@ public partial class RemapButtonNormalState : BasicRemapStateBase
 
     public override void Exit()
     {
+        this.buttonToggleAction.Pressed -= on_buttonToggleAction_Pressed;
         base.Exit();
+    }
+
+    private void on_buttonToggleAction_Pressed()
+    {
+        this.stateMachine.TransitionTo("RemapButtonListeningState");
     }
 }
