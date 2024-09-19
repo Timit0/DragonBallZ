0330285f573750dccabecc9a35ae8ae1e57c25df
TimHaCeff <tim.ha@ceff.ch>
2024-09-19 10:33:08 +0200

    Map update

--- Helpers/ZIndex.cs
+++ Helpers/ZIndex.cs
@@ -2,18 +2,23 @@ public class ZIndex
 {
     public enum ZIndexEnum
     {
-        FLOOR_AND_WATER = -10,
-        DETAILS_ON_FLOOR_UNDER_WATER = -9,
-        WATER = -8,
-        ON_WATER = -7,
-        FLOOR = -6,
-        ON_FLOOR = -5,
-        ACTOR_BEHIND = -4,
-        WALL = -3,
-        ON_WALL = -2,
+        FLOOR_AND_WATER = -14,
+        DETAILS_ON_FLOOR_UNDER_WATER = -13,
+        WATER = -12,
+        ON_WATER = -11,
+        FLOOR = -10,
+        ON_FLOOR = -9,
+        ACTOR_BEHIND = -8,
+        TREE = -7,
+        HOUSE = -6,
+        WALL = -5,
+        ON_WALL = -4,
+        ACTOR_BEHIND_BUT_HOVER_HOUSE = -3,
+        TREE_HOVER_HOUSE = -2,
         DRAGON_BALL = -1,
         ACTOR = 0,
         FIRST_PLANE = 1,
         UI = 2,
+        TRANSITION = 3,
     }
 }
